d3c97dbb770210189d0f5b28cb23a83d643bbfea
Ryan Nguyen <ry.nguyen@mail.utoronto.ca>
2023-06-05 23:42:29 -0400

    Updated README, interface build script

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-Minimal working MATLAB interface for C++.
-Only tested to compile on macOS 12.6.6 (Monterey)
-with clang-1400.0.29.202.
+Minimal working MATLAB interface for C++,
+at least on macOS ....
--- matlab-interface.m
+++ matlab-interface.m
@@ -1,5 +1,5 @@
 mex -setup cpp;
 libName = "rops";
-libFile = "/Users/ryan/Documents/MATLAB/test/bin/static/librops.a";
+libFile = "/Users/ryan/Documents/MATLAB/test/bin/librops.dylib";
 headerFile = "/Users/ryan/Documents/MATLAB/test/include/librops.h";
 clibgen.generateLibraryDefinition(headerFile, "Libraries", libFile, "PackageName", libName);
